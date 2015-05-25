350938e646e2ad3d2498ac3e08532b368e128772
arknave <arnavsastry@gmail.com>
2015-05-25 16:16:16 -0500

    P01

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,7 +1,22 @@
 package org.p99.scala
 
+import java.util.NoSuchElementException
+
 object P01 {
 
-  // TODO
+  // Go with the library definition
+  def last[A](list: List[A]) = list.last
 
+  // But write others in order to learn!
+  def lastIterative[A](list: List[A]) = {
+    if (list.isEmpty) throw new NoSuchElementException
+    list(list.length - 1)
+  }
+
+  // Write in multiple paradigms
+  def lastRecursive[A](list: List[A]): A = list match {
+    case last :: Nil => last
+    case _ :: xs     => lastRecursive(xs)
+    case _           => throw new NoSuchElementException
+  }
 }
--- src/test/scala/P01Spec.scala
+++ src/test/scala/P01Spec.scala
@@ -1,10 +1,23 @@
 package org.p99.scala
 
 import org.scalatest._
+import P01._
 
 class P01Spec extends UnitSpec {
+  "last" should "return the last element" in {
+    assertResult(8) { last(List(1, 1, 2, 3, 5, 8)) }
+    assertResult(5) { last(List(1, 1, 2, 3, 5)) }
+  }
 
-  // TODO
+  it should "also work on all types" in {
+    assertResult('d') { last(List('a', 'b', 'c', 'd')) }
+    assertResult('a') { last(List('a')) }
+  }
 
+  it should "fail on empty lists" in {
+    intercept[NoSuchElementException] {
+      last(List())
+    }
+  }
 }
 
