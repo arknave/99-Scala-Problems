cff4cb43de320654e8fb440c607fe6bffd8dbed0
arknave <arnavsastry@gmail.com>
2015-05-25 16:16:37 -0500

    P02

--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -1,7 +1,16 @@
 package org.p99.scala
 
+import java.util.NoSuchElementException
 object P02 {
 
-  // TODO
+  def penultimate[A](list: List[A]): A = list match {
+    case a :: b :: Nil => a
+    case _ :: xs       => penultimate(xs)
+    case _             => throw new NoSuchElementException
+  }
 
+  def penultimateIterative[A](list: List[A]): A = {
+    if (list.length < 2) throw new NoSuchElementException
+    list(list.length - 2)
+  }
 }
--- src/test/scala/P02Spec.scala
+++ src/test/scala/P02Spec.scala
@@ -1,10 +1,22 @@
 package org.p99.scala
 
 import org.scalatest._
+import P02._
 
 class P02Spec extends UnitSpec {
+  "penultimate" should "return the last but one element of a list" in {
+    assertResult(5) { penultimate(List(1, 1, 2, 3, 5, 8)) }
+    assertResult(3) { penultimate(List(1, 1, 3, 5)) }
+  }
 
-  // TODO
+  it should "work on different types" in {
+    assertResult('d') { penultimate(List('a', 'b', 'c', 'd', 'e')) }
+    assertResult(0.3) { penultimate(List(0.3, 0.5)) }
+  }
 
+  it should "fail on empty or single element lists" in {
+    intercept[NoSuchElementException] { penultimate(List()) }
+    intercept[NoSuchElementException] { penultimate(List(1)) }
+  }
 }
 
